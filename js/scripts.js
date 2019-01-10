d432556b60fc3f647c61ffbf830ed7995bd9ecb3
Stewart ilondanga <stilondanga@gmail.com>
2019-01-10 11:32:34 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,20 +13,21 @@ $(document).ready(function() {
 $("[id^=chkBox]").on("click", function() {
   count();
 });
-/*function count() {
- var amountSelected = 0
- $('#applySelection tr').filter(':has(:checkbox:checked)').each(function() {
-	 amountSelected += Number(($(this).find('[id^=price]').text()).replace(/[\(]/g,'-').replace(/[^0-9\.-]+/g,""));
- });
- var selectedPriceAmount = '$' + amountSelected.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
- $('#totalPrice').text(selectedPriceAmount);
-}
-function allChecked() {
-	 if (document.getElementById("checkAll").checked == true) {
-		 $('[id^=chkBox]').prop('checked', true);
-		 count();
-	 }else {
-		 $('[id^=chkBox]').prop('checked', false);
-		 count();
-	 }
-}
+
+function count() {
+  var amountSelected = 0
+  $('#applySelection tr').filter(':has(:checkbox:checked)').each(function() {
+    amountSelected += Number(($(this).find('[id^=price]').text()).replace(/[\(]/g, '-').replace(/[^0-9\.-]+/g, ""));
+  });
+  /* var selectedPriceAmount = '$' + amountSelected.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+   $('#totalPrice').text(selectedPriceAmount);
+  }
+  function allChecked() {
+  	 if (document.getElementById("checkAll").checked == true) {
+  		 $('[id^=chkBox]').prop('checked', true);
+  		 count();
+  	 }else {
+  		 $('[id^=chkBox]').prop('checked', false);
+  		 count();
+  	 }
+  }
