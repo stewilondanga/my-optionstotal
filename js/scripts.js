1f936629e46a2d17221ab6ba88029d168115b527
Stewart ilondanga <stilondanga@gmail.com>
2019-01-10 11:10:13 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,27 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$(document).ready(function(){
+	 $('.tooltipped').tooltip();
+ });
+$( "[id^=chkBox]" ).on( "click", function() {
+ count();
+});
+function count() {
+ var amountSelected = 0
+ $('#applySelection tr').filter(':has(:checkbox:checked)').each(function() {
+	 amountSelected += Number(($(this).find('[id^=price]').text()).replace(/[\(]/g,'-').replace(/[^0-9\.-]+/g,""));
+ });
+ var selectedPriceAmount = '$' + amountSelected.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ $('#totalPrice').text(selectedPriceAmount);
+}
+function allChecked() {
+	 if (document.getElementById("checkAll").checked == true) {
+		 $('[id^=chkBox]').prop('checked', true);
+		 count();
+	 }else {
+		 $('[id^=chkBox]').prop('checked', false);
+		 count();
+	 }
+}
